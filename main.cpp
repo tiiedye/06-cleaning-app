dd2852dc47531c8e0d3bb8174ff87dcf096959c6
tiiedye <tiiedye@gmail.com>
2020-08-05 14:44:16 -0700

    starting app

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, welcome to Fancies's Carept Cleaning Service." << endl << endl;
+    cout << "How many rooms would you like cleaned?";
+
+    int numberOfRooms {0};
+    cin >> numberOfRooms;
+
+    cout << endl;
+    return 0;
+}
