29d95f1752d1952d8321b33d64c7c935efe6c2ed
tiiedye <tiiedye@gmail.com>
2020-08-05 15:08:00 -0700

    refactoring

--- main.cpp
+++ main.cpp
@@ -9,14 +9,20 @@ int main() {
     int numberOfRooms {0};
     cin >> numberOfRooms;
 
+    const double pricePerRoom {30.00};
+    const double salesTax {0.06};
+
+    // days
+    const int estExp {30}; 
+
     cout << "\nEsimate for carpet cleaning service" << endl;
     cout << "Number of rooms: " << numberOfRooms << endl;
-    cout << "Price per room: $" << 30 << endl;
-    cout << "Cost: $" << 30 * numberOfRooms << endl;
-    cout << "Tax: $" << 30 * numberOfRooms * 0.06 << endl;
+    cout << "Price per room: $" << pricePerRoom << endl;
+    cout << "Cost: $" << pricePerRoom * numberOfRooms << endl;
+    cout << "Tax: $" << pricePerRoom * numberOfRooms * salesTax << endl;
     cout << "=======================" << endl;
-    cout << "Total estimate: $" << (30 * numberOfRooms) + (30 * numberOfRooms * 0.06) << endl;
-    cout << "\nThis estimae is valid for " << 30 << " days." << endl;
+    cout << "Total estimate: $" << (pricePerRoom * numberOfRooms) + (pricePerRoom * numberOfRooms * salesTax) << endl;
+    cout << "\nThis estimae is valid for " << estExp << " days." << endl;
 
     cout << endl;
     return 0;
