e6bdf30ac62556c816d8047766768a5202fdd66b
tiiedye <tiiedye@gmail.com>
2020-08-05 14:54:33 -0700

    finishing up simple program

--- main.cpp
+++ main.cpp
@@ -4,10 +4,19 @@ using namespace std;
 
 int main() {
     cout << "Hello, welcome to Fancies's Carept Cleaning Service." << endl << endl;
-    cout << "How many rooms would you like cleaned?";
+    cout << "How many rooms would you like cleaned? ";
 
     int numberOfRooms {0};
     cin >> numberOfRooms;
+
+    cout << "\nEsimate for carpet cleaning service" << endl;
+    cout << "Number of rooms: " << numberOfRooms << endl;
+    cout << "Price per room: $" << 30 << endl;
+    cout << "Cost: $" << 30 * numberOfRooms << endl;
+    cout << "Tax: $" << 30 * numberOfRooms * 0.06 << endl;
+    cout << "=======================" << endl;
+    cout << "Total estimate: $" << (30 * numberOfRooms) + (30 * numberOfRooms * 0.06) << endl;
+    cout << "\nThis estimae is valid for " << 30 << " days." << endl;
 
     cout << endl;
     return 0;
