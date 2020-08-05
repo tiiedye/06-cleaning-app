7a73e11d12a80a0f6a71cea19c239e419cafdfcd
tiiedye <tiiedye@gmail.com>
2020-08-05 15:29:33 -0700

    making it more complex

--- main.cpp
+++ main.cpp
@@ -3,25 +3,40 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, welcome to Fancies's Carept Cleaning Service." << endl << endl;
-    cout << "How many rooms would you like cleaned? ";
+    int numOfSmRooms {0};
+    int numOfLgRooms {0};
 
-    int numberOfRooms {0};
-    cin >> numberOfRooms;
-
-    const double pricePerRoom {30.00};
+    const double pricePerSmRoom {25.00};
+    const double pricePerLgRoom {35.00};
     const double salesTax {0.06};
 
     // days
     const int estExp {30}; 
 
+    cout << "Hello, welcome to Fancies's Carept Cleaning Service." << endl << endl;
+    
+    cout << "How many SMALL rooms would you like cleaned? ";
+    cin >> numOfSmRooms;
+
+    cout << "How many LARGE rooms would you like cleaned? ";
+    cin >> numOfLgRooms;
+
+    cout << "\n=======================" << endl;
+
     cout << "\nEsimate for carpet cleaning service" << endl;
-    cout << "Number of rooms: " << numberOfRooms << endl;
-    cout << "Price per room: $" << pricePerRoom << endl;
-    cout << "Cost: $" << pricePerRoom * numberOfRooms << endl;
-    cout << "Tax: $" << pricePerRoom * numberOfRooms * salesTax << endl;
+
+    cout << "Number of SMALL rooms: " << numOfSmRooms << endl;
+    cout << "Number of LARGE rooms: " << numOfLgRooms << endl;
+    cout << endl;
+
+    cout << "Price per SMALL room: $" << pricePerSmRoom << endl;
+    cout << "Price per LARGE room: $" << pricePerLgRoom << endl;
+    cout << endl;
+
+    cout << "Cost: $" << (pricePerSmRoom * numOfSmRooms) + (pricePerLgRoom + numOfLgRooms) << endl;
+    cout << "Tax: $" << ((pricePerSmRoom * numOfSmRooms) + (pricePerLgRoom + numOfLgRooms)) * salesTax << endl;
     cout << "=======================" << endl;
-    cout << "Total estimate: $" << (pricePerRoom * numberOfRooms) + (pricePerRoom * numberOfRooms * salesTax) << endl;
+    cout << "Total estimate: $" << ((pricePerSmRoom * numOfSmRooms) + (pricePerLgRoom + numOfLgRooms)) + (((pricePerSmRoom * numOfSmRooms) + (pricePerLgRoom + numOfLgRooms)) * salesTax) << endl;
     cout << "\nThis estimae is valid for " << estExp << " days." << endl;
 
     cout << endl;
